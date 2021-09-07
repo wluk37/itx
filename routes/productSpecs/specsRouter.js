4f983968349fe684130b6c36c83fbef60f31ee97
Wesley Luk <wluk37@gmail.com>
2021-09-07 11:15:55 -0700

    rename and started to design routes for specsRouter

--- routes/productSpecs/specsRouter.js
+++ routes/productSpecs/specsRouter.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../../db/db");
+const tableNames = require("../../db/config");
+
 module.exports = router;
 
 // get all rows of a category
@@ -15,7 +17,16 @@ router.get("/:productID/category/:category", async (req, res, next) => {
   }
 });
 
-router.get("/:productID/");
+router.get("/:productID/", async (req, res, next) => {
+  try {
+    const { PRODUCT_SPECS_TABLENAME, FORM_FACTOR_TABLENAME } = tableNames;
+    let data = await db(PRODUCT_SPECS_TABLENAME).select("*").join(FORM_FACTOR_TABLENAME, function() {
+      this.on()
+    })
+  } catch (error) {
+    
+  }
+});
 
 // create a new product
 router.post("/:productID/create", (req, res, next) => {});
